50516a6ce690e6d9459fa9289fb40120ca6433d7
Pav Kar <pav.kar97@gmail.com>
2021-04-25 23:49:29 +0300

    Update enable_accounts.sh

--- account_operations/enable_accounts.sh
+++ account_operations/enable_accounts.sh
@@ -2,7 +2,7 @@
 while IFS="" read -r p || [ -n "$p" ];
 do
     if id "$p" &>/dev/null; then
-        sudo usermod --expiredate "" "$p"
+        sudo usermod --expiredate '' "$p"
         printf "$p"' got enabled\n'
     else
         printf  'No user named '"$p"'\n'
