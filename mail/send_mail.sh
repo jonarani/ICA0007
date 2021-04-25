000b6a180e9929a430654ec874d2db5aed6f7090
Pav Kar <pav.kar97@gmail.com>
2021-04-25 21:57:10 +0300

    Update send_mail.sh

--- mail/send_mail.sh
+++ mail/send_mail.sh
@@ -1,6 +1,7 @@
+#!bin/bash 
 if [ -z "$1" ]
 then 
-    echo "Empty body. Not sending mail!"
+    echo "Empty body. Not sending mail"
 else
     echo $1 | mail -s "Admin Notification" sysadmin@mail.com
 fi
