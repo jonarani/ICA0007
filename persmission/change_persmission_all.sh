e6dc211dc1cd1c54c77c7d6a1034ebe0069c5a73
Jonathan Rani <jonathanrani@gmail.com>
2021-04-25 23:36:02 +0300

    deleted old

--- persmission/change_persmission_all.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-mapfile -d $'\0' files < <(sudo find "$1" -print0)
-
-for i in "${files[@]}"
-do
-    sudo chmod "$2" "$i" 
-done
--- persmission/change_persmission_specific.sh
+++
@@ -1,7 +0,0 @@
-#!bin/bash
-mapfile -d $'\0' files < <(sudo find "$1" -perm "$2" -print0)
-
-for i in "${files[@]}"
-do
-    sudo chmod "$3" "$i" 
-done
