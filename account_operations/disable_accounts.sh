1450c127874d50af31941f6aaf1e09788a4efeaa
Pav Kar <pav.kar97@gmail.com>
2021-04-25 23:36:20 +0300

    Update disable_accounts.sh

--- account_operations/disable_accounts.sh
+++ account_operations/disable_accounts.sh
@@ -1,11 +1,11 @@
 #!bin/bash
 while IFS="" read -r p || [ -n "$p" ];
 do
-    if id "$1" &>/dev/null; then
+    if id "$p" &>/dev/null; then
         sudo usermod --expiredate 1 "$p"
         printf "$p"' got disabled\n'
     else
         printf  'No user named '"$p"'\n'
     fi
 
-done < naughty_list.txt
+done < "$1"
