e6479ef242f22f8485c71b48bb9d44ecaa7de05e
Jonathan Rani <jorani@ttu.ee>
2021-04-26 13:50:04 +0300

    Validating IP pattern, added colours

--- system/spy.sh
+++ system/spy.sh
@@ -8,6 +8,11 @@ total=$(free | awk '/Mem:/ { print $2 }')           # total RAM
 while IFS= read -r line; do
     user=$(echo "${line}" | awk '{print $1}')
     ip=$(echo "${line}" | awk '{print $2}')
+    
+    # if not IP pattern (for example if it is tmux(31632).%1 then continue)
+    if [[ !($ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$) ]]; then
+        continue
+    fi
 
     echo "User: ${user}"
     echo "IP: ${ip}"
@@ -20,15 +25,28 @@ while IFS= read -r line; do
     if [[ "$me" == "$user" ]] ;
     then
         # ignores commands in top that are labeled "code" or "top"
-        (top -b -n 1 -u "$user" | awk -v CPUS=$cpus -v code="code" -v top="top" 'NR>7  { 
-                                                                                     if($12 != code && $12 != top ) 
+        (top -b -n 1 -u "$user" | awk -v CPUS=$cpus -v code="code" -v top="top" 'NR>7 { 
+                                                                                        if ($12 != code && $12 != top ) 
                                                                                         sum += $9; 
                                                                                     } 
-                                                                                    END { print sum, sum/CPUS; }') &
+                                                                                    END {
+                                                                                        normalized = sum/CPUS 
+                                                                                        if (normalized >= 80)
+                                                                                            print "\033[1;31m" sum, normalized "\033[0m"; 
+                                                                                        else
+                                                                                            print sum, normalized; 
+                                                                                    }') &
         # dont spawn too many processes in parallel
         sleep 0.05
     else
-        (top -b -n 1 -u "$user" | awk -v CPUS=$cpus 'NR>7  { sum += $9; } END { print sum, sum/CPUS; }') &
+        (top -b -n 1 -u "$user" | awk -v CPUS=$cpus 'NR>7  { sum += $9; } 
+                                                     END { 
+                                                        normalized = sum/CPUS
+                                                        if (normalized >= 80)
+                                                            print "\033[1;31m" sum, normalized "\033[0m"
+                                                        else
+                                                            print sum, normalized; 
+                                                     }') &
         # dont spawn too many processes in parallel
         sleep 0.05
     fi
@@ -36,7 +54,14 @@ while IFS= read -r line; do
 
     echo -n "Memory usage: "
     # https://github.com/scaidermern/script-snippets/blob/master/showPerUserMem.sh
-    ps hux -U $user | awk -v total=$total '{ sum += $6} END { printf "%.2f\n", sum / total * 100; }'
+    ps hux -U $user | awk -v total=$total '{ sum += $6} 
+                                           END {
+                                                mem = sum / total * 100
+                                                if (mem >= 50)
+                                                    print "\033[1;31m" mem "\033[0m"
+                                                else
+                                                    printf "%.2f\n", mem; 
+                                           }'
 
     echo
 done <<< "$people"
