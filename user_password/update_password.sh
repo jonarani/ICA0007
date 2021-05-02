ae484256e0c862a62746f15a6f4910c7ead5855d
Pav Kar <pav.kar97@gmail.com>
2021-05-02 20:39:53 +0300

    Update update_password.sh

--- user_password/update_password.sh
+++ user_password/update_password.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 password=$(openssl rand -base64 10)
 sudo yes "$password" | sudo passwd "$1"
-sudo change -M 1 "$n"
+sudo chage -M 1 "$1"
 sudo bash send_mail.sh "New Password: '$password' Expiring in 1 day!" TestingUserForLinux@gmail.com 
