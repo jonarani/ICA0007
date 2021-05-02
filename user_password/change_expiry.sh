c3490b926e3786aa8c71773fa0ff547a1ee158f5
Jonathan Rani <jonathanrani@gmail.com>
2021-05-02 20:48:09 +0300

    fixes

--- user_password/change_expiry.sh
+++ user_password/change_expiry.sh
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 #!bin/bash
 
 # crontab task to check all user's passwords
 
-=======
 #!/bin/bash
->>>>>>> e6925dc2fe2e43321134d20d1865f1bafede98a1
 mapfile -t names < <( getent passwd {1000..6000} | awk -F: '{ print $1}' )
 for n in "${names[@]}"
 do
